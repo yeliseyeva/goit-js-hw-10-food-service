62fe032cfd436a6d4fe9b72af2e11973ec1e18f5
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-26 18:49:55 +0300

    Переключатель темы

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
-import menu from '../menu.json';
-import card from '../templates/card.hbs';
+import menu from './menu.json';
+import card from './templates/card.hbs';
+
+import './styles.css';
+
+// Разметка
 
 const menuList = document.querySelector('.js-menu');
+const menuCardsMarkup = createMenu(menu);
 
-menuList.insertAdjacentHTML('beforeend', createMenu);
+menuList.insertAdjacentHTML('beforeend', menuCardsMarkup);
 
 function createMenu(menu) {
     return menu.map(card).join('');
 }
-console.log(menu)
+
+// Переключатель темы
+
+const checkbox = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body')
+
+
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+themeCheck()
+
+body.classList.add(Theme.LIGHT)
+
+checkbox.addEventListener('click', e => {
+    if (checkbox.checked){
+        body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK)
+    } else {
+        body.classList.remove(Theme.DARK);
+        body.classList.add(Theme.LIGHT);
+        localStorage.setItem('theme', Theme.LIGHT)
+    }
+});
+
+function themeCheck() {
+    const localValue = localStorage.getItem('theme');
+    
+    if(localValue === Theme.DARK) {
+        checkbox.checked = true;
+        body.classList.add(Theme.DARK);
+    }
+};
+
+
