ee5f483a6cefcb5feb5ae2627353e3ca2063ca64
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-26 15:33:20 +0300

    разметка
    /

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './sass/main.scss';
+import menu from '../menu.json'
+import card from '../templates/card.hbs';
+
+const menuList = document.querySelector('.js-menu');
+const menuCardsMarkup = createMenu;
+
+menuList.insertAdjacentHTML('beforeend', createMenu);
+
+function createMenu(menu) {
+    return menu.map(card).join('');
+}
