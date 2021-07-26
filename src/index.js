41915f654faf1e88f3a6f673a223e7fb354a4fd3
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-26 15:41:23 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import menu from '../menu.json'
+import menu from '../menu.json';
 import card from '../templates/card.hbs';
 
 const menuList = document.querySelector('.js-menu');
-const menuCardsMarkup = createMenu;
 
 menuList.insertAdjacentHTML('beforeend', createMenu);
 
 function createMenu(menu) {
     return menu.map(card).join('');
 }
+console.log(menu)
